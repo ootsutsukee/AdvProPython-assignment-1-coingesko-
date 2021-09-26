2c03149adde96253babb3eef40a41b5f47825b6d
ootsutsukee <72724881+ootsutsukee@users.noreply.github.com>
2021-09-27 01:50:47 +0600

    Update coingesko.py

--- src/coingesko.py
+++ src/coingesko.py
@@ -1,71 +1,11 @@
-import math
-import requests
-
-def __get_coins(n):
-    total_pages = 1
-    coins = []
-    pages = [n]
-
-    # Maximum page limit is 250, so if input is greater than 250 we split it into multiple pages.
-
-    if n > 250:
-        pages = []
-
-        # Calculating total pages.
-        total_pages = math.ceil(n / 250)
-        
-        # Filling pages by maximum per page amount. 
-        for page in range(total_pages - 1):
-            pages.append(250)
-        
-        # Adding remaining amount of coins.
-        pages.append(n - 250 * (total_pages - 1))
-
-    # Creating requests for each page.
-    for page in range(total_pages):
-        # Specifying query parameters.
-        query = {
-            'vs_currency': 'usd',
-            'per_page': pages[page],
-            'page': page
-        }
-
-        response = requests.get('https://api.coingecko.com/api/v3/coins/markets', params=query)
-
-        mapped_coins = [coin['name'] for coin in response.json()]
-        
-        coins.extend(mapped_coins)
-
-    return coins
-
-def __format(coins):
-    coins_dict = dict()
-
-    for index in range(len(coins)):
-        coins_dict[index + 1] = coins[index]
-    
-    return coins_dict
-
-def get_coins(n):
-    try:
-        amount = int(n)
-        
-        if amount < 0:
-            return []
-        
-        return __get_coins(amount)
-    except ValueError:
-        return []
-
-def main(n):
-    coins = get_coins(n)
-    formatted_coins = __format(coins)
-
-    for item in formatted_coins.items():
-        print(str(item[0]) + ': ' + str(item[1]))
-    
-    pass
-
-if __name__ == "__main__":
-    n = input("Enter amount of coins: ")
-    main(n)
+from pycoingecko import CoinGeckoAPI 
+import pandas as pd 
+ 
+cg = CoinGeckoAPI() 
+ 
+ 
+def sort_by_market_cap(a): 
+    coin = cg.get_coins_markets(vs_currency='usd') 
+    df_coin = pd.DataFrame(coin, columns=['id', 'market_cap', 'market_cap_rank']) 
+    df_coin.set_index('id', inplace=True) 
+    return df_coin['market_cap_rank'].nsmallest(a)
