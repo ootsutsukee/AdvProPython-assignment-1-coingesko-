0dc98fc89ae6e0e2271584726a6966ba1113cc17
ootsutsukee <72724881+ootsutsukee@users.noreply.github.com>
2021-09-27 01:51:26 +0600

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,23 +1,5 @@
-import sys
-sys.path.append('..')
-
-import unittest
-from src.main import get_coins
-
-class TestCoins(unittest.TestCase):
-    def test_10(self):
-        self.assertEqual(len(get_coins(10)), 10)
-    
-    def test_321(self):
-        self.assertEqual(len(get_coins(321)), 321)
-    
-    def test_string(self):
-        self.assertEqual(len(get_coins('string')), 0)
-    
-    def test_negative(self):
-        self.assertEqual(len(get_coins(-1)), 0)
-    
-    pass
-
-if __name__ == '__main__':
-    unittest.main()
+from main import sort_by_market_cap 
+ 
+val = int(input("Please enter number n:\n")) 
+print(f'Top {val} cryptocurrencies by market capitalization \n') 
+print(sort_by_market_cap(val))
